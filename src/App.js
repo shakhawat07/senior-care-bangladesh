8b667897064d794eaf07c641b73be3878af5bb48
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-10-18 16:23:25 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>sCare Bangladesh</h1>
+      <Banner></Banner>
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const Banner = () => {
+    return (
+        <>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://image.freepik.com/free-photo/cheerful-asian-female-doctor-visiting-senior-caucasian-male-patient-home_1098-20652.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://image.freepik.com/free-photo/old-man-doing-muscle-injury-exercise-using-resistance-band-with-nurse-beside_482257-109.jpg"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://image.freepik.com/free-photo/stomatolog-professioanl-teeth-clinic-smiling-wearing-uniform-looking-camera-dentist-doctor-discussing-with-mother-child-about-teeth-treatment_482257-12010.jpg
+                        "
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
+    );
+};
+
+export default Banner;
